4d64099426581820ecf71bd028e0dd262b137ccc
Horingring <dldldksl@naver.com>
2021-09-08 16:06:00 +0900

    (Front) [Fix] Move View Component into views Folder_1
    작업 내용
    1. view에 해당하는 component를 views folder에 이동 : test
      - 정상적으로 실행됨을 확인

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomePage from "../components/client/common/home/pages/HomePage.vue";
+import HomePage from "@/views/HomePage.vue";
 
 const routes = [
   /* 공통 */
