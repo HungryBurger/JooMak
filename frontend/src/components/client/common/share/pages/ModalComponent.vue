158cc8f7be880a69391046dcd4c9cd8caf9fc562
Jeongwu <park961003@naver.com>
2021-09-15 02:31:31 +0900

    completed ModalComponent

---
+++ frontend/src/components/client/common/share/pages/ModalComponent.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="modal">
+    <div class="overlay" @click="$emit('close')"></div>
+    <div class="modal-card">
+      <header class="modal-header">
+        <slot name="header" />
+        <h1>
+          Title
+          <img
+            id="icon_close"
+            src="@/assets/images/icon_close.svg"
+            alt="icon_close"
+            @click="$emit('close')"
+          />
+        </h1>
+      </header>
+      <div class="modal-content">
+        <slot name="content">
+          <h1>Content</h1>
+        </slot>
+      </div>
+      <footer class="modal-footer">
+        <slot name="footer">
+          <h1>footer</h1>
+          <button id="close-button" @click="$emit('close')">Close</button>
+        </slot>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.modal,
+.overlay {
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  left: 0;
+  top: 0;
+  text-align: center;
+  color: #828282;
+}
+#close-button {
+  cursor: pointer;
+}
+#icon_close {
+  height: auto;
+  width: 12px;
+  position: absolute;
+  top: 12px;
+  right: 12px;
+  color: #828282;
+  cursor: pointer;
+}
+.overlay {
+  opacity: 0.5;
+  background-color: black;
+}
+
+.modal-card {
+  position: relative;
+  max-width: 50%;
+  margin: auto;
+  margin-top: 100px;
+  padding: 20px;
+  background-color: white;
+  min-height: 500px;
+  z-index: 10;
+  opacity: 1;
+}
+header {
+  border-bottom: 2px solid #9e9e9e;
+}
+footer {
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  padding: 12px 0;
+  width: 100%;
+  /* border-top:1px solid; */
+}
+</style>
--- frontend/src/components/client/member/pages/LoginPage.vue
+++ frontend/src/components/client/member/pages/LoginPage.vue
@@ -1,17 +1,39 @@
 <template>
   <div id="loginPage">
     <span>로그인 페이지입니다</span>
+      <button @click="openModal">열기</button>
+    <ModalComponent @close="closeModal" v-if="modal">
+      <template slot="header">
+        header
+      </template>
+      <p>Vue.js Modal Window!</p>
+      <template slot="footer">
+        footer
+      </template>
+    </ModalComponent>
     <br />
     <router-link to="/member/entry">회원가입</router-link>
   </div>
 </template>
 
 <script>
+import ModalComponent from '@/components/client/common/share/pages/ModalComponent.vue'
+
 export default {
+  components: { ModalComponent },
   data() {
-    return {};
+    return {
+       modal:false,
+    };
   },
-  methods: {},
+  methods: {
+      openModal() {
+      this.modal = true
+    },
+    closeModal() {
+      this.modal = false
+    },
+  },
 };
 </script>
 
