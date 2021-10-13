7bd842852bfa3114b8378923b2796f7d7a1a03f8
hwlee0103 <sdiverHyeWon0103@gmail.com>
2021-10-13 19:28:16 +0900

    Change Domain name, add annotations, and add comment
    도메인 명 바꾸기, 어노테이션 추가, 주석 추가

--- BackEnd/src/main/java/com/project/backend/domain/Inventory.java
+++ BackEnd/src/main/java/com/project/backend/domain/Inventory.java
@@ -1,4 +1,18 @@
+package com.project.backend.domain;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Entity		// JPA
+@Getter
+@Setter
+@Table
 public class Inventory {
 	
 	///inventory -> Stock?
@@ -6,23 +20,29 @@ public class Inventory {
 	@Id
 	@GeneratedValue
 	@Column(name = "inventory_id")
-	private int inventoryId;
+	private Long inventoryId;
 	
 	@ManyToOne(fetch = LAZY)
 	@JoinColumn(name = "menu_id")
-	private Menu menu;
+	private MenuBase menubase;
 	
 	//재고수량
+	@Column(name = "inventory_quantity")
 	private int inventoryQuantity;
 	//안전재고
+	@Column(name = "safety_inventory")
 	private int safetyInventory;
 	
 	//등록자
+	@Column(name = "sys_reg_id")
 	private int sysRegId;
 	//등록일
+	@Column(name = "sys_reg_at")
 	private LocalDateTime sysRegAt;
 	//수정자
+	@Column(name = "sys_upd_id")
 	private int sysUpdId;
 	//수정일
+	@Column(name = "sys_upd_at")
 	private LocalDateTime sysUpdAt;
 }
