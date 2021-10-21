a082f6e2e5587ecbc5cf61bd66823c3717001e2a
Horingring <dldldksl@naver.com>
2021-10-21 16:15:48 +0900

    (Front) [Update] Make NumberWithCommaSpan
    작업 내용
    1. NumberWithCommaSpan.vue
      - 개요
        : 1,000단위로 comma(쉼표)를 적용하고 싶을 때 사용
        : span 태그임
      - 사용법
        : 아래 두 방법 모두 사용 가능 ( * 명명 규칙 차이 )
        1) <NumberWithCommaSpan :num="원하는 number"></NumberWithCommaSpan>
        2) <number-with-comma-span :num="원하는 number"></number-with-comma-span>

---
+++ frontend/src/components/client/common/share/components/NumberWithCommaSpan.vue
@@ -0,0 +1,17 @@
+<template>
+  <span>{{ applyNumberWithComma }}</span>
+</template>
+
+<script>
+export default {
+  props: ["num"],
+  computed: {
+    applyNumberWithComma() {
+      let num = this.num;
+      return `+ ${num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`;
+    },
+  },
+};
+</script>
+
+<style></style>
