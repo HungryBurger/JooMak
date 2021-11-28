bd1b13ba76a8589b8e768a272e36bf1e6c0a9135
Horingring <dldldksl@naver.com>
2021-11-28 19:20:22 +0900

    (Front) [Update] Make AddressConfigModal.vue_2
    작업 내용
    1. 주소 설정 Modal 일부 스타일 적용
    2. meber.js
      : 주소록 관련 더미 데이터 작성

--- frontend/src/components/client/common/home/pages/AddressConfigModal.vue
+++ frontend/src/components/client/common/home/pages/AddressConfigModal.vue
@@ -1,9 +1,25 @@
 <template>
-  <modal-component v-if="onAddressConfigModal" @close="closeModal">
+  <modal-component
+    v-if="onAddressConfigModal"
+    @close="closeModal"
+    class="address-config_modal"
+  >
     <template v-slot:header>
       <div class="no-use"></div>
     </template>
-    <template v-slot:content>주소 설정 Modal</template>
+    <template v-slot:content>
+      <h3>배달지 설정</h3>
+      <div class="section_title">현재 주소</div>
+      <div v-if="!currentAddress" class="section_content">
+        <b>* 현재 등록된 주소가 없습니다.</b>
+      </div>
+      <div v-else class="section_content">
+        <div>asdf</div>
+        <div>sdfsdf</div>
+      </div>
+      <br />
+      <div class="section_title">주소 목록</div>
+    </template>
     <template v-slot:footer>
       <div class="no-use"></div>
     </template>
@@ -21,6 +37,7 @@ export default {
   },
   computed: {
     ...mapState("common", ["onAddressConfigModal"]),
+    ...mapState("member", ["currentAddress"]),
   },
   methods: {
     closeModal() {
--- frontend/src/store/modules/member.js
+++ frontend/src/store/modules/member.js
@@ -5,6 +5,29 @@ export const member = {
   state: () => ({
     // currentAddress: "서울특별시 강남구 논현동 123-45",
     currentAddress: "",
+    addressList: [
+      {
+        idx: 1,
+        selected: true,
+        name: "우리집",
+        addressMain: "서울특별시 강남구 논현동 123-45",
+        addressDetail: "3동 1101호",
+      },
+      {
+        idx: 2,
+        selected: false,
+        name: "할머니 댁",
+        addressMain: "",
+        addressDetail: "",
+      },
+      {
+        idx: 3,
+        selected: false,
+        name: "사무실",
+        addressMain: "",
+        addressDetail: "",
+      },
+    ],
   }),
   getters: {},
   mutations: {},
