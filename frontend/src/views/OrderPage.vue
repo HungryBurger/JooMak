0afa7249e2e019014eb5485fb9c659a99a03ae61
Horingring <dldldksl@naver.com>
2022-01-01 20:04:06 +0900

    (Front) [Add] 주문페이지_header 추가
    * 애니메이션 적용

--- frontend/src/views/OrderPage.vue
+++ frontend/src/views/OrderPage.vue
@@ -1,10 +1,15 @@
 <template>
-  <div>
-    주문 & 결제 페이지 입니다<br />
-    현재 주문하려는 항목은
-    <span v-for="(orderForm, i) in orderFormList" :key="i"
-      >{{ orderForm.name }}&nbsp;</span
-    >입니다.
+  <div v-if="orderFormList.length > 0" id="orderPage">
+    <header>
+      <h3>주문 / 결제</h3>
+      <img :src="iconJoomakPath" alt="icon_joomak" class="icon_joomak" />
+      <img :src="iconJoomoPath" alt="icon_joomo" class="icon_joomo" />
+      <img :src="iconChefHatPath" alt="icon_chef-hat" class="icon_chef-hat" />
+    </header>
+    <div class="content">메인영역</div>
+  </div>
+  <div v-else id="orderPage">
+    주문할 상품이 없습니다.
   </div>
 </template>
 
@@ -13,6 +18,15 @@ import { mapState } from "vuex";
 export default {
   computed: {
     ...mapState("order", ["orderFormList"]),
+    iconJoomakPath() {
+      return require("@/assets/images/icon_order_joomak.svg");
+    },
+    iconJoomoPath() {
+      return require("@/assets/images/icon_order_joomo.svg");
+    },
+    iconChefHatPath() {
+      return require("@/assets/images/icon_order_chef-hat.svg");
+    },
   },
   created() {
     console.log(this.orderFormList[0]);
