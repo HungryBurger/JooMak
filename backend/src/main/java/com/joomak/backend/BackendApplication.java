d2b4ada5e24d5656c8bd66fd96f3f7a41b833818
JHJ <hjjooace24@gmail.com>
2022-01-06 23:15:13 +0900

    Config file 수정

--- backend/src/main/java/com/joomak/backend/BackendApplication.java
+++ backend/src/main/java/com/joomak/backend/BackendApplication.java
@@ -2,14 +2,10 @@ package com.joomak.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@EnableWebMvc
 @SpringBootApplication
 public class BackendApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
-
 }
