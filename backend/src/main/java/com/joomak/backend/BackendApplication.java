77784e5810a38831f65bfc7859bedecdd5aa157f
JHJ <hjjooace24@gmail.com>
2022-01-06 22:36:03 +0900

    Add enableMVC Annotation

--- backend/src/main/java/com/joomak/backend/BackendApplication.java
+++ backend/src/main/java/com/joomak/backend/BackendApplication.java
@@ -2,7 +2,9 @@ package com.joomak.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootApplication
 public class BackendApplication {
 
