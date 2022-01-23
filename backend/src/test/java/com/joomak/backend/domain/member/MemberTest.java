7b562c4f28c5a72200370a4f01170989e9ce5089
JHJ <hjjooace24@gmail.com>
2022-01-23 21:51:34 +0900

    Test file 수정

--- backend/src/test/java/com/joomak/backend/domain/member/MemberTest.java
+++ backend/src/test/java/com/joomak/backend/domain/member/MemberTest.java
@@ -1,17 +1,10 @@
 package com.joomak.backend.domain.member;
 
-import com.joomak.backend.util.LocalDateTimeUtil;
-import org.junit.jupiter.api.Test;
-
-import java.time.LocalDateTime;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 
 class MemberTest {
 
-    @Test
-    void test() {
-        LocalDateTimeUtil.convertFormat(LocalDateTime.now());
-    }
+//    @Test
+//    void test() {
+//        LocalDateTimeUtil.convertFormat(LocalDateTime.now());
+    
 }
