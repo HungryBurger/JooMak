ce660554745b1aef27dd13688838510ae5dc42f6
JeongWu <park961003@naver.com>
2022-02-16 23:13:17 +0900

    finished loginpage

--- frontend/src/components/client/member/pages/LoginPage.vue
+++ frontend/src/components/client/member/pages/LoginPage.vue
@@ -1,50 +1,280 @@
 <template>
-  <div id="loginPage">
-    <span>로그인 페이지입니다</span>
-    <button @click="openModal">열기</button>
-    <ModalComponent @close="closeModal" v-if="onModal">
-      <template v-slot:header>
-        <h1>
-          custom header
-        </h1>
-      </template>
-      <template v-slot:content>
-        custom content
-      </template>
-      <template v-slot:footer>
-        custom footer
-      </template>
-    </ModalComponent>
-    <br />
-    <router-link to="/member/entry">회원가입</router-link>
+  <div class="login-page">
+    <div class="form">
+      <img
+        class="login-logo"
+        src="@/assets/images/JoomakLogo_login.svg"
+        alt="JoomakLogo"
+      />
+      <h1>
+        Login
+      </h1>
+      <form class="login-form">
+        <input
+          class="form-control"
+          id="username"
+          type="text"
+          placeholder="username"
+          required
+        />
+
+        <input
+          type="text"
+          class="form-control"
+          id="password"
+          placeholder="password"
+          required
+        />
+        <button class="submit-button" type="submit">login</button>
+        <div class="row c-btn-group c-btn-group--block">
+          <router-link class="col-6 c-btn right-line" to="/member/entry"
+            >회원가입
+          </router-link>
+
+          <a class="col-6 c-btn" type="button" @click="openModal"
+            >아이디/비밀번호 찾기</a
+          >
+          <ModalComponent @close="closeModal" v-if="onModal">
+            <template v-slot:header>
+              <h1>아이디/비밀번호 찾기</h1>
+              <hr />
+            </template>
+            <template v-slot:content>
+              <div class="find_modal_content">
+                <find-modal></find-modal>
+              </div>
+            </template>
+            <template v-slot:footer>
+              <div class="no-use"></div>
+            </template>
+          </ModalComponent>
+          <!-- <a class="c-btn" @click="openModal">비밀번호 찾기</a> -->
+        </div>
+      </form>
+      <div class="social-login-form">
+        <p>소셜 로그인(SNS 로그인)</p>
+        <div class="btn-group">
+          <button class="btn-primary">Naver Login</button>
+          <button class="btn-primary">GOogle Login</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import ModalComponent from "@/components/client/common/share/pages/ModalComponent.vue";
 import { mapState } from "vuex";
+import FindModal from "./FindModal.vue";
+import ModalComponent from "@/components/client/common/share/pages/ModalComponent.vue";
 import { SET_ON_MODAL } from "@/store/modules/common.js";
-
 export default {
-  components: { ModalComponent },
+  components: { FindModal, ModalComponent },
   computed: {
     ...mapState("common", ["onModal"]),
   },
   methods: {
     openModal() {
-      this.$store.commit(`common/${SET_ON_MODAL}`, true);
+      this.$store.commit(`common/${SET_ON_MODAL}`, true); // 주6.
     },
     closeModal() {
-      console.log("close event 발생");
+      console.log("close event 발생"); // 주7.
     },
   },
 };
 </script>
 
 <style scoped>
-#loginPage {
-  width: 500px;
-  height: 300px;
-  background-color: goldenrod;
+.submit-button {
+  text-transform: uppercase;
+  outline: 0;
+  background: #ffc463;
+  border-radius: 5px;
+  /* width: 60%; */
+  border: 0;
+  padding: 12px;
+  color: #ffffff;
+  /* font-size: 12px; */
+  -webkit-transition: all 0.3 ease;
+  transition: all 0.3 ease;
+  cursor: pointer;
+}
+.find_modal_content {
+  margin: 36px;
+}
+
+.form-group {
+  display: flex;
+  flex-direction: row;
+  padding: 20px;
+  width: 70%;
+  margin: 0 auto;
+}
+
+.form-group label {
+  flex: none;
+  display: block;
+  /* font-weight: bold; */
+  font-size: 1em;
+  text-align: left;
+  padding: 12px;
+  /* width: 7em; */
+  margin-right: 0.5em;
+}
+.form-group label.right-inline {
+  text-align: right;
+  padding-right: 8px;
+  padding-left: 10px;
+  /* width: auto; */
+}
+
+.form-group .input-control {
+  flex: 1 1 auto;
+  display: block;
+  margin-bottom: 10px;
+  margin-right: 8px;
+  padding: 4px;
+  margin-top: -4px;
+  width: 60%;
+}
+
+button {
+  width: 60%;
+  padding: 5px 15px;
+  /* min-width: 100px; */
+}
+
+.btn {
+  position: relative;
+  vertical-align: top;
+  height: 40px;
+  width: 25%;
+  padding: 0;
+  font-size: 15px;
+  color: #fff;
+  text-align: center;
+  background: #4d4d4d;
+  border: 0;
+  border-bottom: 2px solid #d0d0d0;
+  cursor: pointer;
+  margin-left: auto;
+}
+a {
+  text-decoration-line: none;
+}
+.c-btn {
+  padding: 0.25em 0.5em;
+  border: solid 1px black;
+  border-radius: 4px;
+  display: inline-block;
+  text-align: center;
+}
+.c-btn:hover,
+.c-btn:focus {
+  cursor: pointer;
+  background-color: #eeeeee;
+  color: #fff;
+}
+.c-btn-group {
+  margin-top: 32px;
+  display: inline-flex;
+  color: #828282;
+  overflow: hidden;
+}
+.c-btn-group--block {
+  display: flex;
+}
+.c-btn-group--block > .c-btn {
+  flex-grow: 1;
+}
+.c-btn-group > .c-btn {
+  color: #828282;
+  border-radius: 0;
+  border: none;
+}
+.c-btn-group > .right-line {
+  border-right: 1px solid #828282;
+}
+h1 {
+  font-size: 24px;
+}
+.social-login-form {
+  margin: 32px auto;
+  width: 60%;
+  text-align: left;
+}
+.social-login-form > p {
+  margin: 12px;
+  color: #828282;
+}
+.btn-group {
+  overflow: hidden;
+  display: flex;
+}
+.btn-group > .btn-primary {
+  border-radius: 5px;
+  background: #ffc463;
+  flex-grow: 1;
+  margin: 12px;
+}
+.btn-group > .btn-primary:nth-child(1) {
+  background: #2ecc71;
+}
+.btn-group > .btn-primary:nth-child(2) {
+  background: #ff5a58;
+}
+.login-page {
+  /* margin: 0; */
+  background-color: #fff0b1;
+  width: 100%;
+  height: 100vh;
+  position: fixed;
+}
+.login-logo {
+  width: 180px;
+  padding: 24px;
+}
+.form {
+  z-index: 1;
+  background: #ffffff;
+  height: 2000px;
+  margin: 0px 120px;
+  padding: 24px;
+  text-align: center;
+}
+.login-form {
+  position: relative;
+  max-width: 540px;
+  margin: 0 auto;
+  padding: 32px;
+  text-align: center;
+}
+.form input {
+  outline: 0;
+  background: #f2f2f2;
+  width: 100%;
+  border: 0;
+  margin: 0 0 24px;
+  padding: 12px;
+  box-sizing: border-box;
+  border-radius: 5px;
+}
+.form button {
+  text-transform: uppercase;
+  outline: 0;
+  background: #ffc463;
+  border-radius: 5px;
+  width: 100%;
+  border: 0;
+  padding: 12px;
+  color: #ffffff;
+  /* font-size: 12px; */
+  -webkit-transition: all 0.3 ease;
+  transition: all 0.3 ease;
+  cursor: pointer;
+}
+.form button:hover,
+.form button:active,
+.form button:focus {
+  /* background: #43a047; */
 }
 </style>
--- frontend/src/components/client/member/pages/MemberEntryPage.vue
+++ frontend/src/components/client/member/pages/MemberEntryPage.vue
@@ -1,20 +1,355 @@
 <template>
-  <div id="memberEntryPage">회원가입 페이지입니다</div>
+  <div class="register-page">
+    <div class="register-page-wrap">
+      <div class="title">
+        <img
+          class="login-logo"
+          src="@/assets/images/JoomakLogo_login.svg"
+          alt="JoomakLogo"
+        />
+        <h1>
+          회원가입
+        </h1>
+      </div>
+      <div class="row justify-content-center">
+        <form class="col-6 g-3 needs-validation" novalidate>
+          <div class="py-3">
+            <label for="username" class="form-label"
+              >아이디<span class="require-test">*</span></label
+            >
+            <div class="input-group">
+              <input
+                type="text"
+                class="form-control"
+                id="username"
+                v-model="username"
+                required
+              />
+
+              <button class="input-button" @click="checkId">
+                &nbsp;&nbsp; 중복 체크 &nbsp;&nbsp;
+              </button>
+              <div class="invalid-feedback">
+                아이디를 입력하세요.
+              </div>
+            </div>
+          </div>
+          <div class="py-3">
+            <label for="password" class="form-label"
+              >비밀번호<span class="require-test">*</span></label
+            >
+            <input
+              type="text"
+              class="form-control"
+              id="password"
+              v-model="password"
+              required
+            />
+            <div class="invalid-feedback">
+              비밀번호를 입력하세요.
+            </div>
+          </div>
+          <div class="py-3">
+            <label for="password-confirm" class="form-label"
+              >비밀번호 확인<span class="require-test">*</span></label
+            >
+            <input
+              type="text"
+              class="form-control"
+              id="password-confirm"
+              v-model="password_confirm"
+              required
+            />
+            <div class="invalid-feedback">
+              비밀번호를 입력하세요.
+            </div>
+          </div>
+          <div class="py-3">
+            <label for="name" class="form-label"
+              >이름<span class="require-test">*</span></label
+            >
+            <input
+              type="text"
+              class="form-control"
+              id="name"
+              v-model="name"
+              required
+            />
+            <div class="invalid-feedback">
+              이름을 입력하세요.
+            </div>
+          </div>
+          <div class="py-3">
+            <label for="email" class="form-label"
+              >이메일<span class="require-test">*</span></label
+            >
+            <div class="input-group mb-3">
+              <input
+                type="text"
+                class="form-control"
+                placeholder="Username"
+                aria-label="Username"
+                required
+              />
+              <span class="input-group-text">@</span>
+              <select id="email-back" class="form-select" required>
+                <option value="" selected>Choose...</option>
+                <option value="naver.com">naver.com</option>
+                <option value="gmail.com">gmail.com</option>
+              </select>
+              <button class="input-button">인증번호 요청</button>
+              <div class="invalid-feedback">
+                이메일을 입력하세요.
+              </div>
+            </div>
+            <div class="input-group">
+              <input
+                type="text"
+                class="form-control"
+                id="email-confirm"
+                placeholder="인증번호 입력"
+                v-model="email_confrim"
+                required
+              />
+              <button class="input-button">인증번호 확인</button>
+              <div class="invalid-feedback">
+                인증번호를 입력하세요.
+              </div>
+            </div>
+          </div>
+          <div class="py-3">
+            <label for="phone" class="form-label"
+              >휴대폰 번호<span class="require-test">*</span></label
+            >
+            <div class="input-group mb-3">
+              <input
+                type="text"
+                id="phone-front"
+                class="form-control"
+                placeholder="010"
+                required
+              />
+              <span class="input-group-text">-</span>
+              <input
+                type="text"
+                class="form-control"
+                id="phone-middle"
+                required
+              />
+              <span class="input-group-text">-</span>
+              <input
+                type="text"
+                class="form-control"
+                id="phone-back"
+                required
+              />
+              <div class="invalid-feedback">
+                휴대폰 번호를 입력하세요.
+              </div>
+            </div>
+          </div>
+        </form>
+        <div class="additional py-3 mb-3">
+          추가 정보
+        </div>
+        <form class="col-6 g-3">
+          <div class="py-3">
+            <label for="name" class="form-label">생년월일</label>
+            <div class="input-group">
+              <select id="year" class="form-select" placeholder="2021">
+                <option selected>Choose...</option>
+              </select>
+              <span class="input-group-text">년</span>
+              <select id="month" class="form-select" placeholder="2021">
+                <option selected>Choose...</option>
+              </select>
+
+              <span class="input-group-text">월</span>
+              <select id="day" class="form-select" placeholder="2021">
+                <option selected>Choose...</option>
+              </select>
+
+              <span class="input-group-text">일</span>
+            </div>
+          </div>
+          <div class="py-3">
+            <label for="nickname" class="form-label">별명</label>
+            <input
+              type="text"
+              class="form-control"
+              id="nickname"
+              v-model="nickname"
+            />
+          </div>
+          <div class="py-3">
+            <label for="gender" class="form-label">성별</label>
+            <div class="input-group mb-3">
+              <div class="form-check form-check-inline">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="inlineRadioOptions"
+                  id="inlineRadio1"
+                  value="option1"
+                />
+                <label class="form-check-label" for="inlineRadio1">1</label>
+              </div>
+              <div class="form-check form-check-inline">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="inlineRadioOptions"
+                  id="inlineRadio2"
+                  value="option2"
+                />
+                <label class="form-check-label" for="inlineRadio2">2</label>
+              </div>
+            </div>
+          </div>
+          <div class="d-grid gap-3 d-md-flex justify-content-md-center">
+            <router-link
+              class="back-button d-grid gap-3 col-6 mx-auto"
+              to="/member/login"
+              >돌아가기
+            </router-link>
+            <button
+              class="submit-button d-grid gap-3 col-6 mx-auto"
+              @click="checkForm"
+            >
+              회원가입
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {};
+    return {
+      errors: {
+        username: "",
+        password: "",
+        password_confirm: "",
+        name: "",
+        phone: "",
+      },
+      username: "",
+      password: "",
+      password_confirm: "",
+      name: "",
+      email: "",
+      email_confirm: "",
+      phone: "",
+      birth: "",
+      nickname: "",
+      gender: "",
+    };
   },
-  methods: {},
+  methods: {
+    checkForm(e) {
+      e.preventDefault();
+      var forms = document.querySelectorAll(".needs-validation");
+      forms.forEach((form) => {
+        console.log(form);
+        if (!form.checkValidity()) {
+          e.preventDefault();
+          e.stopPropagation();
+        }
+        form.classList.add("was-validated");
+      });
+      scrollTo(0, 0);
+    },
+    checkId(e) {},
+  },
 };
 </script>
-
 <style scoped>
-#memberEntryPage {
-  width: 500px;
-  height: 300px;
-  background-color: tomato;
+input[type="radio"] {
+  background-color: #ffffff;
+  border: 1px solid #c0c0c0;
+}
+input[type="radio"]:checked {
+  background-color: #ffc463;
+}
+.require-test {
+  color: #ffc463;
+  font-size: 18px;
+  padding-left: 4px;
+}
+.input-button {
+  text-transform: uppercase;
+  outline: 0;
+  z-index: 1;
+  background: #ffffff;
+  border-radius: 5px;
+  border: 3px solid #ffc463;
+  color: #ffc463;
+  /* font-size: 12px; */
+  -webkit-transition: all 0.3 ease;
+  transition: all 0.3 ease;
+  cursor: pointer;
+}
+.back-button {
+  outline: 0;
+  background: #ffc463;
+  border-radius: 5px;
+  border: 0;
+  color: #ffffff;
+  text-align: center;
+  padding: 12px;
+}
+.submit-button {
+  text-transform: uppercase;
+  outline: 0;
+  background: #ffc463;
+  border-radius: 5px;
+  /* width: 60%; */
+  border: 0;
+  padding: 12px;
+  color: #ffffff;
+  /* font-size: 12px; */
+  -webkit-transition: all 0.3 ease;
+  transition: all 0.3 ease;
+  cursor: pointer;
+}
+.register-page {
+  margin: 0;
+  background-color: #fff0b1;
+}
+.login-logo {
+  width: 180px;
+  padding: 24px;
+}
+.register-page-wrap {
+  z-index: 1;
+  background: #ffffff;
+  height: 2000px;
+  margin: 0px 120px;
+  padding: 24px;
+}
+.title {
+  text-align: center;
+}
+.form input,
+.form select {
+  outline: 0;
+  background: #f2f2f2;
+  width: 100%;
+  border: 0;
+  padding: 12px;
+  box-sizing: border-box;
+  /* font-size: 12px; */
+  border-radius: 5px;
+}
+.additional {
+  text-align: center;
+  color: #828282;
+  border-bottom: 3px solid #ffc463;
+  /* text-decoration: underline #ffc463; */
+  font-size: 18px;
+  width: 55%;
 }
 </style>
