855eacf559d7e0b4df786839f9429e1af246c80a
JHJ <hjjooace24@gmail.com>
2022-03-02 17:58:03 +0900

    [회원]Member Controller 수정

--- backend/src/main/java/com/joomak/backend/controller/MemberController.java
+++ backend/src/main/java/com/joomak/backend/controller/MemberController.java
@@ -13,17 +13,19 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/members")
 public class MemberController {
     private final MemberService memberService;
 
     // 회원번호로 한명의 회원 조회
-    @GetMapping(value = "/members/{memberId}", produces = {MediaType.APPLICATION_JSON_VALUE})
-    public ResponseEntity<Member> getMembers(@PathVariable("memberId") Long memberId) {
+    @GetMapping(value = "/{memberId}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<Member> getMember(@PathVariable("memberId") Long memberId) {
         Member member = memberService.findById(memberId);
         return ResponseEntity.ok(member);
     }
+
     // 모든 회원 조회
-    @GetMapping(value = "/members/getMemberAll", produces = {MediaType.APPLICATION_JSON_VALUE})
+    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<List<Member>> getAllMembers() {
         List<Member> member = memberService.findAll();
         return new ResponseEntity<List<Member>>(member, HttpStatus.OK);
@@ -32,14 +34,14 @@ public class MemberController {
     /**
      * HttpServletRequest가 아닌 Custom object를 파라미터로 받을것.
      */
-    @PostMapping(value = "/member")
+    @PostMapping
     public ResponseEntity<Member> save(Member member) {
         return ResponseEntity.ok(memberService.save(member));
     }
 
     // 특정 회원 밴 처리(악성유저처리)
-    @PutMapping(value = "/members/ban/{memberId}")
-    public ResponseEntity<Member> ban(@PathVariable Long memberId){
+    @PutMapping(value = "ban/{memberId}")
+    public ResponseEntity<Member> ban(@PathVariable Long memberId) {
         return ResponseEntity.ok(memberService.ban(memberId));
     }
 }
--- backend/src/main/java/com/joomak/backend/domain/member/Store.java
+++ backend/src/main/java/com/joomak/backend/domain/member/Store.java
@@ -1,19 +1,17 @@
 package com.joomak.backend.domain.member;
 
 
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
-import static lombok.AccessLevel.PACKAGE;
 import static lombok.AccessLevel.PROTECTED;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = PROTECTED)
-@AllArgsConstructor(access = PACKAGE)
 public class Store {
 
     @Id
@@ -39,4 +37,16 @@ public class Store {
 
     private int minOrderPrice;
 
+    @Builder
+    public Store(Long id, Member member, String storeName, Double grade, String previewStoreIntro, String businessHour, String mainComment, String subComment, int minOrderPrice) {
+        this.id = id;
+        this.member = member;
+        this.storeName = storeName;
+        this.grade = grade;
+        this.previewStoreIntro = previewStoreIntro;
+        this.businessHour = businessHour;
+        this.mainComment = mainComment;
+        this.subComment = subComment;
+        this.minOrderPrice = minOrderPrice;
+    }
 }
