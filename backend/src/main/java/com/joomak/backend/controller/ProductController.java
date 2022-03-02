1e8d72449dfaa080db64a8e3fe0712eda500eace
JHJ <hjjooace24@gmail.com>
2022-03-02 17:58:28 +0900

    Prodcuct Error 수정

--- backend/src/main/java/com/joomak/backend/controller/ProductController.java
+++ backend/src/main/java/com/joomak/backend/controller/ProductController.java
@@ -1,18 +1,17 @@
 package com.joomak.backend.controller;
 
-import java.util.List;
-
+import com.joomak.backend.domain.product.Product;
+import com.joomak.backend.service.ProductService;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import com.joomak.backend.domain.product.Product;
-import com.joomak.backend.service.ProductService;
+import java.util.List;
 
 public class ProductController {
-	private final ProductService productService = new ProductService();
+    private final ProductService productService = new ProductService();
 
     // 상품번호로 상품 한개 조회
     @GetMapping(value = "/products/{productId}", produces = {MediaType.APPLICATION_JSON_VALUE})
@@ -20,21 +19,21 @@ public class ProductController {
         Product product = productService.findById(productId);
         return ResponseEntity.ok(product);
     }
-    
+
     // 전체 상품 조회
     @GetMapping(value = "/products/all", produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<List<Product>> getProductsAll() {
         List<Product> product = productService.findAll();
         return ResponseEntity.ok(product);
     }
-    
+
     // 매장에 등록된 모든 상품 조회
-    @GetMapping(value = "/products/{stordId}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    @GetMapping(value = "/products/{storeId}", produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<List<Product>> getStoreProducts(@PathVariable("storeId") Long storeId) {
-    	List<Product> storeProducts = productService.findStoreProducts(storeId);
+        List<Product> storeProducts = productService.findStoreProducts(storeId);
         return ResponseEntity.ok(storeProducts);
     }
-    
+
     // 상품 등록
     @PostMapping(value = "/product")
     public ResponseEntity<Product> save(Product product) {
--- backend/src/main/java/com/joomak/backend/repository/ProductRepository.java
+++ backend/src/main/java/com/joomak/backend/repository/ProductRepository.java
@@ -1,15 +1,12 @@
 package com.joomak.backend.repository;
 
+import com.joomak.backend.domain.product.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-import com.joomak.backend.domain.product.Product;
-
-@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
-    List<Product> findStoreProduct(Long storeId);
-
+    List<Product> findByStore_Id(@Param(value = "storeId")Long storeId);
 }
