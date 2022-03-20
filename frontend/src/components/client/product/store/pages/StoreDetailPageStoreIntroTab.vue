6ead13ad1c103ac0d1e1d364e3dd49c903ed78be
JeongWu <park961003@naver.com>
2022-03-20 23:58:25 +0900

    apply kakao map marker(certain address)

--- frontend/src/components/client/product/store/pages/StoreDetailPageStoreIntroTab.vue
+++ frontend/src/components/client/product/store/pages/StoreDetailPageStoreIntroTab.vue
@@ -164,6 +164,13 @@
 
 <script>
 export default {
+  data() {
+      return {
+        place:'경기도 성남시 분당구 정자동 17-5',
+        placeName:'KFC 부평시장역점'
+          // map: null
+      };
+  },
   methods: {
      initMap () {
       const container = document.getElementById('map');
@@ -172,13 +179,48 @@ export default {
         level: 3
       }
       const map = new kakao.maps.Map(container, options)
+      //마커추가하려면 객체를 아래와 같이 하나 만든다. 
+      var marker = new kakao.maps.Marker({ position: map.getCenter() }); 
+      marker.setMap(map)
+      
+      //주소-좌표 변환 객체를 생성합니다
+      var geocoder = new kakao.maps.services.Geocoder();
+      // 주소로 좌표를 검색합니다
+      geocoder.addressSearch(this.place, (result, status)=> {
+
+          // 정상적으로 검색이 완료됐으면 
+          if (status === kakao.maps.services.Status.OK) {
+
+              var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+
+              // 결과값으로 받은 위치를 마커로 표시합니다
+              var marker = new kakao.maps.Marker({
+                  map: map,
+                  position: coords
+              });
+
+              // 인포윈도우로 장소에 대한 설명을 표시합니다
+              var infowindow = new kakao.maps.InfoWindow({
+                  content: '<div style="width:150px;text-align:center;padding:6px 0;">'+ this.placeName+'</div>'
+              });
+              infowindow.open(map, marker);
+
+              // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+              map.setCenter(coords);
+          } 
+      });    
+
+      
       // const markerPosition = new kakao.maps.LatLng(35.19656853772262, 129.0807270648317);
 
       // const marker = new kakao.maps.Marker({
       //   position: markerPosition
       // });
       // marker.setMap(map)
-  },
+    },
+    // displayMarker(){
+
+    // },
   },
    mounted() {
      if (window.kakao && window.kakao.maps) {
@@ -187,7 +229,7 @@ export default {
         const script=document.createElement('script');
         script.onload=()=>kakao.maps.load(this.initMap);
         script.src =
-        'http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=b6cbac052860ed6350771411150f7480';
+        '//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=b6cbac052860ed6350771411150f7480&libraries=services';
         document.head.appendChild(script);
       }
   },
