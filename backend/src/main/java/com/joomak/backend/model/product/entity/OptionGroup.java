319d9975d24e7d6241bae6f90fac484041277e31
hwlee0103 <sdiverHyeWon0103@gmail.com>
2022-09-26 22:24:23 +0900

    Create OptionGroup Entity & OptionGroupType Enum

---
+++ backend/src/main/java/com/joomak/backend/model/product/entity/OptionGroup.java
@@ -0,0 +1,42 @@
+package com.joomak.backend.model.product.entity;
+
+import com.joomak.backend.model.common.BaseEntity;
+import com.joomak.backend.model.product.enums.OptionGroupType;
+import com.joomak.backend.model.product.enums.OptionState;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "OptionGroup")
+@Getter
+@EqualsAndHashCode(callSuper = false)
+public class OptionGroup extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "option_group_id")
+    private Long id;
+
+    @Column(name = "option_group_name")
+    private String optionGroupName;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "option_group_type")
+    private OptionGroupType optionGroupType;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "option_state")
+    private OptionState optionState;
+
+    @Column(name = "option_max_selection")
+    private int optionMaxSelection;
+
+    @Column(name = "option_min_selection")
+    private int optionMinSelection;
+
+    @Column(name = "is_mandatory")
+    private Boolean isMandatory;
+    
+}
