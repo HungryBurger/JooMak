6dee869f11aab47178f5c918669f6f6820dd952d
주형진 <hjjooace24@gmail.com>
2022-11-23 16:37:46 +0900

    cors filter 수정 및 error message 수정 및 설정 수정

--- backend/src/main/java/com/joomak/backend/exception/ErrorMessage.java
+++ backend/src/main/java/com/joomak/backend/exception/ErrorMessage.java
@@ -8,7 +8,7 @@ import lombok.RequiredArgsConstructor;
 public enum ErrorMessage {
 
     NOT_ENOUGH_STOCK("재고가 충분하지 않습니다."),
-    ALREADY_EXISTS_MEMBER("은 이미 존재하는 회원입니다"),
+    ALREADY_EXISTS_EMAIL("이미 존재하는 이메일입니다"),
     ;
 
     private final String errorMessage;
